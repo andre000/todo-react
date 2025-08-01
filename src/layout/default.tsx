2288a4921f87235431f794e089eecea81281e374
André Adriano <a000.andre@gmail.com>
2025-08-01 15:08:39 -0300

    feat: restructure Default layout to include Title component for improved UI

--- src/layout/default.tsx
+++ src/layout/default.tsx
@@ -1,7 +1,16 @@
+import { Title } from '../components/Title/Title'
+
 export function Default({ children }: React.PropsWithChildren) {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-        {children}
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+      <div className="w-full max-w-4xl p-6">
+        <Title className="mb-6 mx-auto w-full">
+          Todo App
+        </Title>
+        <div className="max-w-4xl w-full mx-auto">
+          {children}
+        </div>
+      </div>
     </div>
   );
 }
