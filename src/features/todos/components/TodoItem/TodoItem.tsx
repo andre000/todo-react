a75fbf6b8fad0b3c94c0a357c4567da9726f83c9
André Adriano <a000.andre@gmail.com>
2025-08-01 18:11:25 -0300

    feat: add delete functionality to TodoItem component

--- src/features/todos/components/TodoItem/TodoItem.tsx
+++ src/features/todos/components/TodoItem/TodoItem.tsx
@@ -1,12 +1,22 @@
 import type { TodoItemProps } from "./TodoItem.types";
+import { useTodos } from "../../../../store/todos";
 
 export const TodoItem: React.FC<TodoItemProps> = ({ todo, className }) => {
+  const { removeTodo } = useTodos();
+
   const handleDragStart = (event: React.DragEvent<HTMLDivElement>) => {
     event.dataTransfer.setData('text/plain', todo.id);
   };
 
+  const handleDelete = () => {
+    removeTodo(todo.id);
+  };
+
   return (
-    <div className={`shadow-md rounded-lg p-4 mb-4 cursor-move ${className}`} draggable onDragStart={handleDragStart}>
+    <div className={`shadow-md rounded-lg p-4 mb-4 cursor-move relative ${className}`} draggable onDragStart={handleDragStart}>
+      <span className="text-gray-400 hover:text-gray-600 cursor-pointer absolute top-2 right-4" onClick={handleDelete}>
+        &times;
+      </span>
       <p className="text-gray-600">{todo.text}</p>
       <div className="mt-2">
         {todo.tags.map(tag => (
