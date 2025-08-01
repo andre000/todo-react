f9d5a4930fe4a4c625f907ecae9bd66b13a26263
André Adriano <a000.andre@gmail.com>
2025-08-01 16:36:13 -0300

    feat: wrap App component with TodosProvider for state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { Default } from './layout/default'
 import { TodoList } from './features/todos/components/TodoList/TodoList'
 import './styles/globals.css'
+import { TodosProvider } from './store/todos'
 
 function App() {
   return (
-    <Default>
-      <div className='flex gap-4'>
-        <TodoList title='To do' />
-        <TodoList title='In progress' />
-        <TodoList title='Done' />
-      </div>
-    </Default>
+    <TodosProvider>
+      <Default>
+        <div className='flex gap-4'>
+          <TodoList title='To do' />
+          <TodoList title='In progress' />
+          <TodoList title='Done' />
+        </div>
+      </Default>
+    </TodosProvider>
   )
 }
 
