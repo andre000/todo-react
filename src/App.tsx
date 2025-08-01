c3dcf1cde9f0e0af83ac527e10c7f9d8d2cb1811
André Adriano <a000.andre@gmail.com>
2025-08-01 14:57:25 -0300

    feat: implement Title component for consistent heading styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
-import { useState } from 'react'
+import { Title } from './components/Title/Title'
 import { Default } from './layout/default'
 import './styles/globals.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Default>
-      <div className="h-screen flex flex-col items-center justify-center gap-4">
-        <h1 className='text-4xl font-bold'>Vite + React</h1>
-        <button onClick={() => setCount((count) => count + 1)} className="bg-blue-500 text-white px-4 py-2 rounded">
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
+      <Title level={1}>Todo App</Title>
     </Default>
   )
 }
---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,24 @@
+import clsx from "clsx";
+
+type HeadingLevel = 1 | 2 | 3 | 4;
+
+const sizes: Record<HeadingLevel, string> = {
+  1: 'text-4xl',
+  2: 'text-3xl',
+  3: 'text-2xl',
+  4: 'text-xl',
+}
+
+interface TitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
+  level?: HeadingLevel;
+}
+
+export const Title: React.FC<TitleProps>  = ({ children, level = 1, className, ...rest }) => {
+  const Tag = `h${level}` as `h1` | `h2` | `h3` | `h4`;
+
+  return (
+    <Tag className={clsx(`text-gray-800 font-sans font-bold`, sizes[level], className)} {...rest}>
+      {children}
+    </Tag>
+  );
+}
